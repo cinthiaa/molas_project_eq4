3fa955f69b50b7cb34eb1600c9827021202698f9
Cinthia Abigail <a01281280@tec.mx>
2025-11-12 19:36:43 -0600

    Updated fixture file

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -4,10 +4,9 @@ from pathlib import Path
 import pandas as pd
 import pytest
 
-ROOT = Path(__file__).resolve().parents[1]
-src_path = ROOT / "src"
-if src_path not in sys.path:
-    sys.path.insert(0, str(src_path))
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+if str(PROJECT_ROOT) not in sys.path:
+    sys.path.insert(0, str(PROJECT_ROOT))
 
 
 @pytest.fixture
